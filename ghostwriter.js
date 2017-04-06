4ad0d16525ba6f938d8f471ca55421d0067eaf3c
Cory Wall <cwall4@mail.csuchico.edu>
2017-04-06 12:33:07 -0700

    completed writeTweet function

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,6 +1,17 @@
 function writeTweet()
 {
-  
+  console.log("in writeTweet");
+  var randIndex, sentence;
+  var intro = ["Have you ever noticed", "It's funny how", "Why is it okay that", "Why is it that"];
+  var body = ["the government", "the weather", "cafeteria food", "Chico State", "your cellphone", "your mom"];
+  var end = ["is plotting against you.", "is spying on you.", "is controling your mind."];
+  randIndex = randomUpTo(intro.length - 1);
+  sentence = intro[randIndex];
+  randIndex = randomUpTo(body.length - 1);
+  sentence += " " + body[randIndex];
+  randIndex = randomUpTo(end.length - 1);
+  sentence += " " + end[randIndex];
+  document.getElementById("tweet").innerHTML = sentence;
 }
 
 //Generates a random whole number between 0 and max (inclusive)
